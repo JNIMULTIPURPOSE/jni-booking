cd8cda5e9df6399e436589e88a4a2af3785d2b24
JNIMULTIPURPOSE <jnimultipurpose001@gmail.com>
2026-05-24 03:35:22 -0500

    fix mongodb startup

--- server/server.js
+++ server/server.js
@@ -33,19 +33,19 @@ app.get("/", (req, res) => {
 });
 
 /* PORT */
+
+/* DATABASE */
 const PORT = process.env.PORT || 5000;
 
-/* DATABASE CONNECTION */
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
     console.log("MongoDB Error:", err);
   });
-
-/* START SERVER (ALWAYS RUNS) */
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
